59ce6a63a20e19735141d755967e2939b2c68514
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-01-19 01:58:03 -0600

    added

--- React/zustand/src/App.jsx
+++ React/zustand/src/App.jsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { create } from "zustand";
+
+const useStore = create((set) => ({
+  counter: 0,
+  bears: 0,
+  increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+  removeAllBears: () => set({ bears: 0 }),
+  increment: () => set((state) => ({ counter: state.counter + 1 })),
+  decrement: () => set((state) => ({ counter: state.counter - 1 })),
+}));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const {
+    counter,
+    increment,
+    decrement,
+    bears,
+    increasePopulation,
+    removeAllBears,
+  } = useStore();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <p>Counter: {counter}</p>
+      <p>Bears: {bears}</p>
+      <button onClick={increment}>Increment C</button>
+      <button onClick={decrement}>DecrementC</button>
+      <button onClick={increasePopulation}>Iecrement B</button>
+      <button onClick={removeAllBears}>set B 0</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
