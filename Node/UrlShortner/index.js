1a8c63ab1a7d9f8a1dc908d1c908d69bb3be6713
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-02-05 09:44:49 -0600

    redux file

---
+++ Node/UrlShortner/index.js
@@ -0,0 +1,84 @@
+const dotenv = require("dotenv")
+const express = require("express");
+const { connectToMongoDB } = require("./connect");
+const urlRoute = require("./routes/url");
+const URL = require("./models/url");
+
+const app = express();
+dotenv.config();
+
+
+const PORT = process.env.PORT || 8000;
+const MONGODB_URL = process.env.MONGODB_URL
+connectToMongoDB(MONGODB_URL).then(() =>
+  console.log("Mongodb connected")
+);
+
+app.use(express.json());
+app.use("/url", urlRoute);
+
+app.get("/:shortId", async (req, res) => {
+  const shortId = req.params.shortId;
+  console.log("Requested shortId:", shortId);
+
+  const entry = await URL.findOneAndUpdate(
+    { shortID: shortId },
+    { $inc: { visitHistory: 1 } },
+    { new: true }
+  );
+
+  if (entry) {
+    // Document found, log and redirect
+    console.log("Entry:", entry);
+    res.redirect(entry.redirectURL);
+  } else {
+    // Document not found, handle accordingly (e.g., send an error response)
+    console.log("Entry not found for shortId:", shortId);
+    res.status(404).send("Not Found");
+  }
+});
+
+
+app.listen((PORT), () => {
+    console.log(`Server Running on port ${PORT}`);
+})
+
+// const express = require("express");
+// const { connectToMongoDB } = require("./connect");
+// const urlRoute = require("./routes/url");
+// const URL = require("./models/url");
+
+// const app = express();
+// const PORT = 8000;
+
+// connectToMongoDB("mongodb://localhost:27017/shortUrl").then(() =>
+//   console.log("Mongodb connected")
+// );
+
+// app.use(express.json());
+
+// app.use("/url", urlRoute);
+
+// app.get("/:shortId", async (req, res) => {
+//   const shortId = req.params.shortId;
+//   console.log("Requested shortId:", shortId);
+
+//   const entry = await URL.findOneAndUpdate(
+//     { shortID: shortId },
+//     { $inc: { visitHistory: 1 } },
+//     { new: true }
+//   );
+
+//   if (entry) {
+//     // Document found, log and redirect
+//     console.log("Entry:", entry);
+//     res.redirect(entry.redirectURL);
+//   } else {
+//     // Document not found, handle accordingly (e.g., send an error response)
+//     console.log("Entry not found for shortId:", shortId);
+//     res.status(404).send("Not Found");
+//   }
+// });
+   
+
+// app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
