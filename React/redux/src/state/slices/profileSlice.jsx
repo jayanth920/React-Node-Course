4cd2651a2d3262886c01bd8ba46c1000720f5f14
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-02-05 09:44:49 -0600

    redux file

---
+++ React/redux/src/state/slices/profileSlice.jsx
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+export const profileSlice = createSlice({
+    name:"profile",
+    initialState : { value: { name: "", age: ""}},
+    reducers: {
+        login: (state, action) => {
+            state.value = action.payload
+        },
+        logout: (state, action) => {
+            state.value = {name: "", age: ""}
+        }
+    }
+
+})
+
+export default profileSlice.reducer
+export const {login, logout} = profileSlice.actions
