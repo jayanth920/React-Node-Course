e2697686be2cb90f3a4d4e4c34c54c81c99f3af2
jayanth920 <113549918+jayanth920@users.noreply.github.com>
2024-03-23 14:29:25 -0500

    onclick button debounce search

---
+++ React/customHook/src/App.tsx
@@ -0,0 +1,54 @@
+// import { MouseEventHandler, useState } from "react";
+// import "./App.css";
+// import useCounter from "./hooks/useCounter";
+// import React from "react";
+
+// function App() {
+
+//   const [count, increment, decrement, reset] = useCounter();
+
+//   return (
+//     <div className="App">
+//       <div className="counterContainer">
+//         Counter: {String(count)}
+//         <button onClick={increment as MouseEventHandler<HTMLButtonElement>}>Add</button>
+//         <button onClick={decrement as MouseEventHandler<HTMLButtonElement>}>Subtract</button>
+//         <button onClick={reset as MouseEventHandler<HTMLButtonElement>}>Reset</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React from 'react';
+import useFetch from './hooks/useFetch';
+import { useState } from 'react';
+
+const FetchData = () => {
+  const [term, setTerm] = useState("london");
+  const [debouncedTerm, setDebouncedTerm] = useState("london");
+
+  const handleButtonClick = () => {
+    setDebouncedTerm(term); // Update debounced term when button is clicked
+  };
+
+  const url = `http://api.weatherapi.com/v1/current.json?key=31456b301eb5447e9e062613242303&q=${debouncedTerm}&aqi=yes`;
+  const { isLoading, isError, data: user } = useFetch(url);
+
+  console.log(user);
+  console.log(url);
+
+  return (
+    <div className='border-4 border-green-500 bg-amber-300'>
+      <input type="text" onChange={(e) => setTerm(e.target.value)} />
+
+      <button onClick={handleButtonClick}>Fetch Data</button>
+
+      {(user as any)?.location.name}
+    </div>
+  );
+};
+
+export default FetchData;
